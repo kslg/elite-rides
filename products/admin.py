4661c6f699b09d04881b5403363fdbd2c823612b
Krishan <kslg257@gmail.com>
2022-12-12 21:19:00 +0000

    Created new products app, models and fixtures

---
+++ products/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'make',
+        'model',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
