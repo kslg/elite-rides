8056cac63492fb1af6ed01ec4dac4508024150e5
Krishan <kslg257@gmail.com>
2022-12-18 12:40:33 +0000

    Completed webhook handler and added the round function to the delivery_cost in the Order Model

--- checkout/models.py
+++ checkout/models.py
@@ -22,6 +22,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -36,10 +38,12 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = round(self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100, 2)
         else:
             self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
+        print(self.order_total)
+        print(self.delivery_cost)
+        self.grand_total = (self.order_total + self.delivery_cost)
         self.save()
 
     def save(self, *args, **kwargs):
