accbfe9a00315f8d24fa2c2d2f08e4fd40506fea
Krishan <kslg257@gmail.com>
2023-01-09 23:19:49 +0000

    Setup and add new elephant sql database to the project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn elite_rides.wsgi:application
--- elite_rides/settings.py
+++ elite_rides/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-b3vdsl77!v9nht#6nx3!rxc0yj-mz()%x*o%__d3!z!tec=4(y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pp5-elite-rides.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -127,13 +129,17 @@ WSGI_APPLICATION = 'elite_rides.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
