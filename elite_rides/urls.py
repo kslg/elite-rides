48e5fe4f98af6bfb53f4b2081f9d6848c3b9ed0a
Krishan <kslg257@gmail.com>
2023-01-17 12:08:38 +0000

    Added a custom 404 error page

--- elite_rides/urls.py
+++ elite_rides/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from contact import views as contact_views
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +36,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'elite_rides.views.handler404'
