6dfc52728844dddcaceed460d1abda2b807fa8da
Krishan <kslg257@gmail.com>
2023-02-24 18:02:05 +0000

    fixing flake8 F811 error

--- elite_rides/urls.py
+++ elite_rides/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from contact import views as contact_views
-from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
